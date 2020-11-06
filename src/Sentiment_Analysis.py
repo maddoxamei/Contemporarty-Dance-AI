4eedaccab52c33c76f7661648ca372b1e659762c
Mei Maddox <maddoxamei@gmail.com>
2020-11-06 11:36:06 -0500

    Update Sentiment_Analysis.py

--- src/Sentiment_Analysis.py
+++ src/Sentiment_Analysis.py
@@ -1,3 +1,4 @@
+#pip install flair, requires PyTorch use command from https://pytorch.org/
 from flair.data import Sentence
 from flair.models import TextClassifier
 analyzer = TextClassifier.load('en-sentiment')
